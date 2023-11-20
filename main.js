6ebf7494fc01a9c9c1aa9d091c2dbc35c90effed
Javierjmc <javierjosemartinezcedeno@gmail.com>
2023-11-20 10:02:56 -0400

    Primer elemento del cuerpo

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const iconoBurger=document.getElementById('icono-hamburguesa');
 const menu=document.getElementById('menu');
+const contenedorPrincipal=document.getElementById('contenedor-principal')
 
 iconoBurger.addEventListener('click', ()=>{
     menu.classList.toggle('translate-y-[0]')
+    contenedorPrincipal.classList.toggle('translate-y-[0]')
     iconoBurger.classList.toggle('invert')
 }
 );
